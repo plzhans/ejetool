33512c65514f278617f2540b6df95b443ecb913e
손원철 <plzhans@colorverse.co>
2024-09-04 14:17:15 +0900

    스프링 부트 어드민 리버스 프록시 관련 설정

--- backend/ejetool-admin/ejetool-admin-spring/src/main/java/com/ejetool/admin/spring/config/SecurityConfig.java
+++ backend/ejetool-admin/ejetool-admin-spring/src/main/java/com/ejetool/admin/spring/config/SecurityConfig.java
@@ -5,19 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.util.StringUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import com.ejetool.admin.spring.filter.CustomAccessDeniedHandler;
-import com.ejetool.admin.spring.filter.CustomAuthenticationEntryPoint;
 
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import lombok.RequiredArgsConstructor;
@@ -78,7 +72,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
-       // config.setAllowCredentials(true);
+        config.setAllowCredentials(true);
         config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
